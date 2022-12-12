d83f08b8b37f3c83d9759ef2a6e174e895bf52ec
“Salil48” <salilchharia@gmail.com>
2022-12-12 22:05:48 +0530

    Added: Modify the earlier UC 2 to continue till either of them have won 21 times. Show if it’s a Win or Tie

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,6 +1,6 @@
 echo "Welcome to Flip Coin Simulation Problem"
 
-for ((i=0; i<100; i++))
+for ((i=0; i<45; i++))
 do
 r=$((RANDOM%2))
 if [[ $r -eq 0 ]]
@@ -10,3 +10,30 @@ else
 echo "Tail"
 fi
 done
+
+if [[ $r -eq 0 ]]
+then
+echo "h=heads"
+h=1
+while [ $h -le 21 ]
+do
+echo "Head"
+((h++))
+done
+echo "head win"
+elif [ $r -eq 1 ]
+then
+echo "t=tails"
+t=1
+while [ $t -le 21 ]
+do
+echo "Tail"
+((t++))
+done
+echo "tail win"
+else
+echo " It is Tie"
+fi
+
+
+
